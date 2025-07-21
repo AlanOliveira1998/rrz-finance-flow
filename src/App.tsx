d32663646dbd9c2d49b8d5cad3767d94c54068a3
Alan Oliveira <79263845+AlanOliveira1998@users.noreply.github.com>
2025-07-20 22:15:12 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { AuthProvider } from "@/hooks/useAuth";
-// import { InvoicesProvider } from "@/hooks/useInvoices";
-// import { ProjectsProvider } from "@/hooks/useProjects";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
