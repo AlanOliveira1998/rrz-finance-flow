142e9e58aa3578a2446a648d195c723896ad7f47
Alan Oliveira <79263845+AlanOliveira1998@users.noreply.github.com>
2025-07-21 15:09:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,25 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
 import { InvoicesProvider } from "@/hooks/useInvoices";
+import { ClientsProvider } from "@/hooks/useClients";
+import { ProjectsProvider } from "@/hooks/useProjects";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
   <AuthProvider>
-    <InvoicesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </InvoicesProvider>
+    <ClientsProvider>
+      <ProjectsProvider>
+        <InvoicesProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </InvoicesProvider>
+      </ProjectsProvider>
+    </ClientsProvider>
   </AuthProvider>
 );
 
