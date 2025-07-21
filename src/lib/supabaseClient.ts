c3d38aa3606f4349a93eec9298085b357f094df2
Alan Oliveira <79263845+AlanOliveira1998@users.noreply.github.com>
2025-07-21 17:29:50 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qzsxayglyekpzxljvjdc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6c3hheWdseWVrcHp4bGp2amRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4NzA1NDksImV4cCI6MjA2ODQ0NjU0OX0.wBx12-Y-gm8DojxIwUtnjRUUkeeKgzxph6APD0cCde4';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
