59482a76f26a3597e0fea738350d07654666c065
Alan Oliveira <79263845+AlanOliveira1998@users.noreply.github.com>
2025-07-20 22:08:22 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
 import { InvoicesProvider } from "@/hooks/useInvoices";
-import { ClientsProvider } from "@/hooks/useClients";
 import { ProjectsProvider } from "@/hooks/useProjects";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -19,16 +18,14 @@ const App = () => (
       <AuthProvider>
         <ProjectsProvider>
           <InvoicesProvider>
-            <ClientsProvider>
-              <Toaster />
-              <Sonner />
-              <BrowserRouter>
-                <Routes>
-                  <Route path="/" element={<Index />} />
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </BrowserRouter>
-            </ClientsProvider>
+            <Toaster />
+            <Sonner />
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
           </InvoicesProvider>
         </ProjectsProvider>
       </AuthProvider>
